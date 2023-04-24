572324df6d9fee9d1a2e114bfac1b0d1b50f006a
Tiffany Fust <tiffanyfust@gmail.com>
2023-04-23 20:16:36 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './assets/UltrasLogo.png';
+import tagline from './assets/Casualwearstore.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <img src={logo} alt="The ULtras Logo"></img>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
