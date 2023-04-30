b03fe979ce319f9686b9529e877c67cfea0fff4e
Tiffany Fust <tiffanyfust@gmail.com>
2023-04-30 14:32:19 -0700

    added main page structure

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faLinkedin, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import backgroundImg from './assets/sliderImage1.jpg';
 
 
 function App() {
@@ -14,10 +16,10 @@ function App() {
       <header>
         <div className='alertBar'>
           <ul>
-            <li><FontAwesomeIcon icon="fa-brands fa-facebook-f" /></li>
-            <li><FontAwesomeIcon icon="fa-brands fa-instagram" /></li>
-            <li><FontAwesomeIcon icon="fa-brands fa-linkedin-in" /></li>
-            <li><FontAwesomeIcon icon="fa-brands fa-twitter" /></li>
+            <li><FontAwesomeIcon icon={faFacebook} /></li>
+            <li><FontAwesomeIcon icon={faInstagram} /></li>
+            <li><FontAwesomeIcon icon={faLinkedin} /></li>
+            <li><FontAwesomeIcon icon={faTwitter} /></li>
             <li></li>
           </ul>
           <div id='news'>
@@ -50,6 +52,39 @@ function App() {
           <li id='cart'><FontAwesomeIcon icon={faBagShopping} /></li>
         </menu>
       </header>
+
+{/*Slider Section */}
+
+      <section id='landingSlider'>
+        <div>
+          <img src={backgroundImg}></img>
+        </div>
+        <div id='sliderText'>
+          <h1>Summer Collection.</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim massa diam elementum habitant fames ac penatibus et.</p>
+          <a href='#'>Shop It Now →</a>
+        </div>
+      </section>
+
+{/*Featured Products*/}
+
+      <section id='featuredProducts'>
+        <div>
+          <h2>Featured Products</h2>
+          <a href='#'>View All Products →</a>
+        </div>
+        <ul>
+          <li>
+            <div>
+              <img src=''></img>
+            </div>
+            <div>
+              <h3>Full Sleeve Cover Shirt</h3>
+              <p>$ 40.00</p>
+            </div>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
